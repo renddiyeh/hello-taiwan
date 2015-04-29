f57f4a32ed98185e3083890ab349de7bb530c9f0
Renddi <renddi@relab.cc>
2015-04-30 02:24:28 +0800

    bootstrap

--- assets/custom-js/food-map.js
+++ assets/custom-js/food-map.js
@@ -5,11 +5,14 @@ d3.json("js/twCounty2010merge.topo.json", function(error, data) {
 
     console.log(topodata);
 
-  	var projection = d3.geo.mercator().center([120.979531, 23.978567]).scale(10000);
+  	var projection = d3.geo.mercator().center([120.979531, 23.978567]).scale(50000);
 
  	var geoPath = d3.geo.path().projection(projection);
 
   	d3.select("svg#map").selectAll("path").data(topodata.features).enter()
-  		.append("path").attr("d", geoPath);
+  		.append("path").attr("d", geoPath)
+  		.attr("title", function(d) { return d.properties.name });
 
+  	$("#map path").tooltipsy();
 });
+
