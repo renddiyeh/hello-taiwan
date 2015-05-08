3338dfe4b497e216fb05dafe30e2ff4487698a29
Renddi <renddi@relab.cc>
2015-05-09 00:55:21 +0800

    stable 0.1

--- assets/custom-js/main.js
+++ assets/custom-js/main.js
@@ -7,7 +7,7 @@ $(function() {
         var page = State.data.name;
         $.fn.fullpage.moveTo(page);     	
     });
-    History.replaceState({page:0, name: 'hello'}, null, '?hello-taiwan');
+    History.replaceState({page:0, name: 'hello'}, null, '?hello');
 
 	$.fn.fullpage.setMouseWheelScrolling(false);
     $.fn.fullpage.setAllowScrolling(false);
