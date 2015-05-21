d6800f21d2c23d76ef935ee316f9840bf284fefa
Renddi <renddi@relab.cc>
2015-05-21 22:56:37 +0800

    summary

--- api/controllers/VoterController.js
+++ api/controllers/VoterController.js
@@ -8,10 +8,29 @@
 module.exports = {
 	summary: function (req, res) {
 		Voter.find().populate('q1_1').exec(function(err, found) {
-			var result = [0,0,0,0,0];
+			var result = {
+				q1_1: [0,0,0,0,0],
+				q1_2: [0,0,0,0,0],
+				q3_1: [0,0,0,0,0,0],
+				q3_2: [0,0,0,0,0,0],
+				duration: [0,0,0,0]
+			};
+			var vLength = found.length;
 			async.each(found, function(voter, callback) {
-				var l = voter['q1_1'].length;
-				result[l-1] += 1;
+				var q1_1 = voter['q1_1'].length;
+				var q1_2 = voter['q1_2'].length;
+				var q3_l = voter['q3_1'].length;
+				var q3_2 = voter['q3_2'].length;				
+				result['q1_1'][q1_1-1] += 1;
+				result['q1_2'][q1_2-1] += 1;
+				result['q3_1'][q3_1] += 1;
+				result['q3_2'][q3_2] += 1;
+
+				var duration = voter.duration || [0,0,0,0];
+				for (var i = duration.length - 1; i >= 0; i--) {
+					result.duration[i] += duration[i]/vLength;
+				};
+
 				callback();
 			}, function(err) {
 				res.json(result)
